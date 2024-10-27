8a9523223283d3c105ef59041442e1d71f0f949c
Mr-wilz <juiciwhilf@gmail.com>
2024-10-27 08:07:11 +0000

    10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,14 +1,9 @@
 export default function updateUniqueItems(groceriesMap) {
-  // Check if the argument is a Map, otherwise throw an error
-  if (!(groceriesMap instanceof Map)) {
-    throw new Error("Cannot process");
-  }
+  if (!(groceriesMap instanceof Map)) throw new Error("Cannot process");
 
-  // Update entries where the value is 1
   groceriesMap.forEach((value, key) => {
-    if (value === 1) {
-      groceriesMap.set(key, 100);
-    }
+    if (value === 1) groceriesMap.set(key, 100);
   });
+
   return groceriesMap;
 }
